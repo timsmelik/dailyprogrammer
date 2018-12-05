5e1bff709bfd734a8897d2c9e9a992bbcfbe6810
Tim <timsmelik@gmail.com>
2018-12-05 17:25:18 +0100

    Added silly solutions for challenge #369 (easy)

--- challenge_369_easy.py
+++ challenge_369_easy.py
@@ -3,18 +3,30 @@ Challenge #369 (easy)from /r/dailyprogrammer
 https://old.reddit.com/r/dailyprogrammer/comments/a0lhxx/20181126_challenge_369_easy_hex_colors/
 """
 
+from functools import reduce
+
 def hexcolor1(red, green, blue):
     """Most straightforward solution"""
     return '#{:02x}{:02x}{:02x}'.format(red, green, blue)
 
+# Fun/bonus solutions below, not very useful
+
 def hexcolor2(*vals):
-    """Use arbitrary number of arguments. Not useful in practice, but fun"""
+    """Use arbitrary number of arguments"""
     return ('#' + '{:02x}' * len(vals)).format(*vals)
 
 #One-liner
 hexcolor3 = lambda r, g, b : '#{:02x}{:02x}{:02x}'.format(r, g, b)
 
-hexcolor = hexcolor1
+def hexcolor4(r, g, b):
+    """Use map"""
+    return '#' + ''.join(map(lambda x : '{:02x}'.format(x), (r, g, b)))
+
+def hexcolor5(r, g, b):
+    """Use reduce"""
+    return reduce(lambda a, b : a + '{:02x}'.format(b), ('#', r, g, b))
+
+hexcolor = hexcolor5
 
 TEST_INPUT = [
     hexcolor(255, 99, 71),
